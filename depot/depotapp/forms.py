fa3961104fe356bb23d862a56f1fdef8f4e375eb
“paomian” <tanglnan1994@gmail.com>
2013-03-26 19:42:22 +0800

    init

---
+++ depot/depotapp/forms.py
@@ -0,0 +1,30 @@
+
+from django import forms
+from models import *
+import itertools
+
+
+def anyTure(predicate,sequence):
+		return True in itertools.imap(predicate,sequence)
+def endswith(s,*endings):
+	return anyTure(s.endswith,endings)
+class ProductForm(forms.ModelForm):
+	
+    class Meta:
+        model = Product	
+        # exclude = [] # uncomment this line and specify any field to exclude it from the form
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+		
+	def clean_prince(self):
+		price=self.clean_data['price']
+		if price<=0:
+			raise forms.ValidationError("price always >0")
+		return price
+
+	def clean_image_url(self):
+		url=self.cleaned_data['image_url']
+		if not endswith(url,'.jpg','.png','.gif'):
+			raise forms.ValidationError("type error")
+		return url
---
+++ depot/depotapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+	title=models.CharField(max_length=100,unique=True)
+	description=models.TextField()
+	image_url=models.CharField(max_length=200)
+	price=models.DecimalField(max_digits=8,decimal_places=2)
